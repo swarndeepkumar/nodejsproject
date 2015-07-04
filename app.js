38955d495c22d101f46e9ebd70f1ca3332564786
unknown <swarndeep.kumar@ntz-014.sebiz.net>
2015-07-04 13:18:56 +0530

    add express in code

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var http = require('http');
-http.createServer(function(req,res){
+var http    = require('http');
+var express = require('express');
+var app     = express();
+/*http.createServer(function(req,res){
 	res.end("yahooo");
 }).listen(3000,'localhost');
+*/
+app.get('/home',function(req,res){
+	res.send("I have got the get response");
+
+});
+
 console.log('Server is running at localhost:3000');
