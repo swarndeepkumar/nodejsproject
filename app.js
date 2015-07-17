99a31afc698778118dd3c3d223582940c4f75dbd
unknown <swarndeep.kumar@gmail.com>
2015-07-17 19:18:10 +0530

    add sesion and cookies

--- app.js
+++ app.js
@@ -1,70 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
- 
-function send404(response) {
-	response.writeHead(404, { 'Content-Type': 'text/plain' });
-	response.write('Error 404: Resource not found.');
-	response.end();
-}
- 
-var mimeLookup = {
-	'.js': 'application/javascript',
-	'.html': 'text/html'
-};
- 
-var server = http.createServer(function (req, res) {
- 
-if (req.method == 'GET') {
- 
-	// resolve file path to filesystem path
-	var fileurl;
-	//if (req.url == '/') fileurl = '/index.html';
-	//else fileurl = req.url;
-	switch(req.url){
-		case '/':
-			fileurl = '/index.html';
-			break;
-		case '/form':
-			fileurl = '/form.html';
-			break;
-        case '/contact':
-			fileurl = '/contact.html';
-			break;
-		default:
-			fileurl = req.url;
-
-	}
-	
-	var filepath = path.resolve('./public' + fileurl);
-	
-	// lookup mime type
-	var fileExt = path.extname(filepath);
-	var mimeType = mimeLookup[fileExt];
-	if (!mimeType) {
-		send404(res);
-		return;
-	}
-	
-	// see if we have that file
-	fs.exists(filepath, function (exists) {
-	    // if not
-		if (!exists) {
-			send404(res);
-			return;
-		};
-		 
-		// finally stream the file
-		res.writeHead(200, { 'content-type': mimeType });
-		
-		fs.createReadStream(filepath).pipe(res);
-	});
-
-
-
-}
-else {
-	send404(res);
-}
-}).listen(3000);
-console.log('server running on port 3000');
+var express = require('express');
+var timeout = require('connect-timeout');
+var app		= express()
+				.use('/api',
+					function(req, res, next){
+					// Simulate a hanging request by doing nothing
+						console.log("First Call back");
+						res.end("Response End Successfully");
+					}
+					)
+				.listen(3000);
